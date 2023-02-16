b6de567efd88856e3d3c37d5a1d96bcb7da0498b
sfwnisme <sfwnisme@gmail.com>
2023-02-16 16:41:43 +0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let nav = document.querySelector("nav");
 let navIcon = document.querySelector(".nav-icon");
+let navPath = document.querySelector(".nav-icon svg path:last-of-type");
 let navCont = document.querySelector("nav ul");
 let navSocial = document.querySelector("nav ul li.nav-social");
 let logo = document.querySelector("nav figure img");
@@ -11,10 +12,10 @@ navIcon.addEventListener("click", (e) => {
   navIcon.firstElementChild.classList.toggle("active");
   // logo.src = "./assets/images/logo-white.png";
   logo.src = "./assets/images/logo.png";
+  navPath.setAttribute("d", "M4 16l16 0");
   if (!navCont.classList.contains("active")) {
     logo.src = "./assets/images/logo-white.png";
+    navPath.setAttribute("d", "M4 16l07 0");
   }
-  navSocial.classList.toggle("nav-social-active")
+  navSocial.classList.toggle("nav-social-active");
 });
-
-console.log(logo);
